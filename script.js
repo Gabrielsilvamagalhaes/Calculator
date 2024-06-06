7ea5cd1b1417a11785dd16e60cd562900ca8b0cc
Gabriel Silva <gabiles278@gmail.com>
2024-06-06 18:13:19 -0300

    calculator created

---
+++ script.js
@@ -0,0 +1,37 @@
+function insertValues(numb) {
+    const result = document.getElementById('result');
+    result.innerHTML += numb;
+        
+}
+
+function calculate() {
+    const result = document.getElementById('result');
+    if(result) {
+       let refactorFloatNumber = eval(result.innerText);
+        refactorFloatNumber = refactorFloatNumber.toFixed(2);
+        result.innerHTML = refactorFloatNumber;
+    }
+}
+
+function squareCalculate() {
+    const result = document.getElementById('result');
+    const resultValue = result.innerText;
+    console.log(Math.sqrt(Number(resultValue)));
+    if(isNaN(Number(resultValue))) {
+        clean();
+        return alert('Insira apenas um número sem operador!');
+    }
+    result.innerHTML =  '' + Math.sqrt(Number(resultValue));
+}
+
+function back() {
+    const result = document.getElementById('result');   
+    const text = result.innerText;
+    result.innerHTML = text.slice(0, - 1);
+}
+
+function clean() {
+    const result = document.getElementById('result');
+    result.innerHTML =' ';
+}
+
